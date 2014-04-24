d7e6add0c4f5ee7e4776b87fc45c6661f009be57
Tim Holt <timholt@Tims-MacBook-Air.local>
2014-04-24 18:10:47 -0400

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Tim Holt on 4/24/14.
+//  Copyright (c) 2014 Tim Holt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
